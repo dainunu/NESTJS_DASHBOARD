0b80c9ed63d42f264bdd2abc0b013289724ece58
loop <584816202@qq.com>
2023-12-27 14:17:47 +0800

    feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
-import "@/app/ui/global.css";
+import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('http://localhost:3000'),
+};
 
 export default function RootLayout({
   children,
